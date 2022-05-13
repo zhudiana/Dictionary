83699e8072ea9c3bdbc329a75b3133ea1df000d5
Yodit Fentaw <yoditfentaw90@gmail.com>
2022-05-13 02:29:14 -0700

    ---

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+      );
+
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
+  return <div className="App">dictionmaertgrefvl</div>;
 }
 
 export default App;
